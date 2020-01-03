cfc1c727c2533dcfe52219aad292bb3184240bbc
programmerarunkumar <programmerarunkumar@gmail.com>
2020-01-03 19:09:11 +0530

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+var sum=0;
+function checkFive(num)
+{
+  if(num != 5)
+  {
+    return ' not equal';
+  }
+  else
+  {
+    return 'equal';
+  }
+}
+console.log(checkFive('5'));
+console.log(sum);
